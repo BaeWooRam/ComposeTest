a4f974438c66724993525cf4b27f9e747faf1f71
BWooRam <wrbae@hyundaiht.co.kr>
2023-01-13 10:36:53 +0900

    [Update] Data 관련 코드 추가 - ApiBuilder, Rss Api Interface 추가 - dto 추가 - 테스트 코드 추가 - build gradle 패키지들 추가

---
+++ app/src/main/java/com/geekstudio/composetest/data/dto/Item.kt
@@ -0,0 +1,23 @@
+package com.geekstudio.composetest.data.dto
+
+import org.simpleframework.xml.Element
+import org.simpleframework.xml.Root
+
+@Root(name = "item", strict = false)
+data class Item (
+    @field:Element(name = "title")
+    @param:Element(name = "title")
+    val title: String = "",
+
+    @field:Element(name = "description")
+    @param:Element(name = "description")
+    val description: String = "",
+
+    @field:Element(name = "pubDate")
+    @param:Element(name = "pubDate")
+    val pubDate: String = "",
+
+    @field:Element(name = "link")
+    @param:Element(name = "link")
+    val link: String = ""
+)
---
+++ app/src/main/java/com/geekstudio/composetest/data/remote/GetNewsRss.kt
@@ -0,0 +1,28 @@
+package com.geekstudio.composetest.data.remote
+
+import com.geekstudio.composetest.data.api.RssApi
+import com.geekstudio.composetest.data.dto.Rss
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class GetNewsRss(private val rssService: RssApi) {
+
+    /**
+     * 언어 타입에 따라 RSS 들고 오기
+     */
+    fun getAll(languageType: RssApi.LanguageType): Flow<Rss> {
+        return flow{
+            val item = when (languageType) {
+                RssApi.LanguageType.EN -> {
+                    rssService.getEnNewsList()
+                }
+                RssApi.LanguageType.KR -> {
+                    rssService.getKrNewsList()
+                }
+            }
+
+            emit(item)
+        }
+    }
+}
+
