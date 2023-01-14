21fd60b992298abd3e73c7e47141e5a73cc3d061
BaeWooRam <betagogo@naver.com>
2023-01-14 21:55:11 +0900

    [Update] UI 업데이트 - ListView Card, 클릭이벤트, Text 관련 코드 추가

--- app/src/main/java/com/geekstudio/composetest/presentation/main/MainActivity.kt
+++ app/src/main/java/com/geekstudio/composetest/presentation/main/MainActivity.kt
@@ -2,24 +2,19 @@ package com.geekstudio.composetest.presentation.main
 
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
 import com.geekstudio.composetest.data.dto.Rss
 import com.geekstudio.composetest.presentation.base.BaseActivity
 import com.geekstudio.composetest.presentation.base.BaseUiState
 import com.geekstudio.composetest.ui.theme.ComposeTestTheme
-import com.geekstudio.composetest.ui.view.Greeting
 import com.geekstudio.composetest.ui.view.RssList
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity() {
@@ -63,7 +58,7 @@ class MainActivity : BaseActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    RssList(rss)
+                    RssList(this, rss)
                 }
             }
         }
--- app/src/main/java/com/geekstudio/composetest/ui/view/ListView.kt
+++ app/src/main/java/com/geekstudio/composetest/ui/view/ListView.kt
@@ -1,42 +1,83 @@
 package com.geekstudio.composetest.ui.view
 
-import android.text.Html
+import android.content.Context
 import android.util.Log
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import android.widget.Toast
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.layout
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.geekstudio.composetest.data.dto.Item
 import com.geekstudio.composetest.data.dto.Rss
-import com.geekstudio.composetest.ui.theme.ComposeTestTheme
 import com.geekstudio.composetest.ui.theme.DefaultMargin
 import com.geekstudio.composetest.utils.Util
 
 @Composable
-fun RssList(rss: Rss) {
+fun RssList(context: Context, rss: Rss) {
     val channel = rss.channel
-    LazyColumn() {
+    val padding = PaddingValues(horizontal = DefaultMargin, vertical = 8.dp)
+    LazyColumn(
+        contentPadding = padding
+    ) {
         itemsIndexed(
             channel.item
         ) { index, item ->
-            ItemCard(item)
+            ItemCard(item) {
+                Toast.makeText(context, "RssList onClick item = $item", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
 
 @Composable
-private fun ItemCard(item: Item){
-    Column {
-        Text(text = Util.decodingHtml(item.title))
-        Text(text = Util.decodingHtml(item.description))
+private fun ItemCard(item: Item, onClick: () -> Unit){
+    Card(
+        modifier = Modifier
+            .padding(horizontal = 8.dp, vertical = 8.dp)
+            .fillMaxWidth()
+            .clickable { onClick() },
+        elevation = 2.dp,
+        backgroundColor = Color.White,
+        shape = RoundedCornerShape(corner = CornerSize(16.dp)),
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = DefaultMargin, vertical = DefaultMargin),
+        ) {
+            Text(
+                fontSize = 20.sp,
+                fontFamily = FontFamily.SansSerif,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Start,
+                maxLines = 1,
+                text = Util.decodingHtml(item.title),
+                overflow = TextOverflow.Ellipsis
+            )
+            Spacer(modifier = Modifier.height(8.dp).fillMaxWidth())
+            Text(
+                fontSize = 12.sp,
+                fontFamily = FontFamily.SansSerif,
+                fontWeight = FontWeight.Medium,
+                textAlign = TextAlign.Start,
+                maxLines = 2,
+                text = Util.decodingHtml(item.description),
+                overflow = TextOverflow.Ellipsis
+            )
+        }
     }
 }
